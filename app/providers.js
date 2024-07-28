16e12f31ff4cc8ea90ec4b2cc7640e46cbc1b60d
Leonardo Gomes <lgpy17@gmail.com>
2024-07-28 10:09:07 +0100

    identify users

--- app/providers.js
+++ app/providers.js
@@ -2,12 +2,29 @@
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
+const getCharNames = () => {
+  const chars = window.localStorage.getItem("characters")
+  if (!chars) return []
+  const jsonChars = JSON.parse(chars)
+  const characters = jsonChars.state.characters
+  return characters.map((char) => char.name) || []
+}
+
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: 'https://la-utilsv2.vercel.app/ingest',
     ui_host: 'https://eu.posthog.com',
     person_profiles: 'always',
   })
+  const chars = getCharNames();
+  if (chars.length > 0) {
+    posthog.identify(
+        chars[0],
+        {
+            characters: chars
+        }
+    )
+  }
 }
 export function CSPostHogProvider({ children }) {
     return <PostHogProvider client={posthog}>{children}</PostHogProvider>
