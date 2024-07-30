c909aa8f72fbcae2df2e956785d4ed00fa7b7747
Leonardo Gomes <lgpy17@gmail.com>
2024-07-30 10:53:31 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+            // This will match all routes
+            source: '/(.*)',
+            headers: [
+                {
+                key: 'Access-Control-Allow-Origin',
+                value: 'https://la-utils.vercel.app', // Allow only this origin
+                },
+                {
+                key: 'Access-Control-Allow-Methods',
+                value: 'GET, POST, PUT, DELETE, OPTIONS',
+                },
+                {
+                key: 'Access-Control-Allow-Headers',
+                value: 'Content-Type, Authorization',
+                },
+            ],
+            },
+        ];
+    },
 };
 export default nextConfig
