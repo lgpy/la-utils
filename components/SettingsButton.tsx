a950659cbe1d120e1b30389af2d8d6e9bf241a01
Leonardo Gomes <lgpy17@gmail.com>
2024-10-02 18:05:38 +0100

    Refactor NavBar and add server selection dropdown

--- components/SettingsButton.tsx
+++ components/SettingsButton.tsx
@@ -18,9 +18,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
+import { useServerStore } from "@/providers/ServerProvider";
 
 export default function SettingsButton() {
   const { setTheme, theme } = useTheme();
+  const { store: serverStore } = useServerStore((s) => s);
   const router = useRouter();
 
   const ThemeIcon = (() => {
@@ -81,6 +83,126 @@ export default function SettingsButton() {
                   <SunMoon className="mr-2 h-4 w-4" />
                   <span>System</span>
                 </DropdownMenuCheckboxItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuGroup>
+        <DropdownMenuGroup>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger className="focus:text-background data-[state=open]:text-background">
+              <span>Server</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger className="focus:text-background data-[state=open]:text-background">
+                    NAW
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Thaemine"}
+                        onCheckedChange={() =>
+                          serverStore.setServer("Thaemine")
+                        }
+                      >
+                        <span>Thaemine</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Brelshaza"}
+                        onCheckedChange={() =>
+                          serverStore.setServer("Brelshaza")
+                        }
+                      >
+                        <span>Brelshaza</span>
+                      </DropdownMenuCheckboxItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger className="focus:text-background data-[state=open]:text-background">
+                    NAE
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Luterra"}
+                        onCheckedChange={() => serverStore.setServer("Luterra")}
+                      >
+                        <span>Luterra</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Balthorr"}
+                        onCheckedChange={() =>
+                          serverStore.setServer("Balthorr")
+                        }
+                      >
+                        <span>Balthorr</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Nineveh"}
+                        onCheckedChange={() => serverStore.setServer("Nineveh")}
+                      >
+                        <span>Nineveh</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Inanna"}
+                        onCheckedChange={() => serverStore.setServer("Inanna")}
+                      >
+                        <span>Inanna</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Vairgrys"}
+                        onCheckedChange={() =>
+                          serverStore.setServer("Vairgrys")
+                        }
+                      >
+                        <span>Vairgrys</span>
+                      </DropdownMenuCheckboxItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger className="focus:text-background data-[state=open]:text-background">
+                    EUC
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Ortuus"}
+                        onCheckedChange={() => serverStore.setServer("Ortuus")}
+                      >
+                        <span>Ortuus</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Elpon"}
+                        onCheckedChange={() => serverStore.setServer("Elpon")}
+                      >
+                        <span>Elpon</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Ratik"}
+                        onCheckedChange={() => serverStore.setServer("Ratik")}
+                      >
+                        <span>Ratik</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Arcturus"}
+                        onCheckedChange={() =>
+                          serverStore.setServer("Arcturus")
+                        }
+                      >
+                        <span>Arcturus</span>
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem
+                        checked={serverStore.name === "Gienah"}
+                        onCheckedChange={() => serverStore.setServer("Gienah")}
+                      >
+                        <span>Gienah</span>
+                      </DropdownMenuCheckboxItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
               </DropdownMenuSubContent>
             </DropdownMenuPortal>
           </DropdownMenuSub>
