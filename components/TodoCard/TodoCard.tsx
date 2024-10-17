d88754229cf410772cb12d4c972e895f51726c78
Leonardo Gomes <lgpy17@gmail.com>
2024-10-17 04:30:57 +0100

    Adjust display of raid and task counts in TodoCard component

--- components/TodoCard/TodoCard.tsx
+++ components/TodoCard/TodoCard.tsx
@@ -112,10 +112,20 @@ export default function TodoCard({ char, isGoldEarner }: Props) {
       <Tabs defaultValue="raids">
         <TabsList className="w-full bg-background/30 p-0 h-auto rounded-none">
           <TabsTrigger value="raids" className="w-full rounded-none">
-            Raids ({Object.keys(char.assignedRaids).length})
+            <p>
+              Raids{" "}
+              <span className="text-xs text-muted-foreground">
+                ({Object.keys(char.assignedRaids).length})
+              </span>
+            </p>
           </TabsTrigger>
           <TabsTrigger value="tasks" className="w-full rounded-none">
-            Tasks ({char.tasks.length})
+            <p>
+              Tasks{" "}
+              <span className="text-xs text-muted-foreground">
+                ({char.tasks.length})
+              </span>
+            </p>
           </TabsTrigger>
         </TabsList>
         <Separator />
