9b4c096d231e8131d4481d99d047fae5a78b008d
lgpy <lgpy17@gmail.com>
2025-06-16 22:42:14 +0100

    fix: remove unnecessary callbackURL from Discord sign-in

--- components/SettingsButton.tsx
+++ components/SettingsButton.tsx
@@ -97,7 +97,6 @@ export default function SettingsButton() {
 						onClick={() =>
 							authClient.signIn.social({
 								provider: "discord",
-								callbackURL: `${window.location.origin}/`,
 							})
 						}
 					>
