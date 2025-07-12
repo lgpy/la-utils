5417c752f4b61412aa7a486e8b0afa3169903ae6
lgpy <lgpy17@gmail.com>
2025-07-12 19:11:13 +0100

    fix: add @prisma/nextjs-monorepo-workaround-plugin dependency to package.json and bun.lock

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 import BundleAnalyzer from  '@next/bundle-analyzer';
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
   }
 }
  
