b85db219681ee9d6979a3a3659ccfd0f67b925ee
lgpy <lgpy17@gmail.com>
2025-08-31 22:14:30 +0100

    hide number tasks incomplete for types that dont have any task assigned

--- components/TodoCard/TodoCardsSeparateTasksButton.tsx
+++ components/TodoCard/TodoCardsSeparateTasksButton.tsx
@@ -35,12 +35,15 @@ export default function TodoCardsSeparateTasksButton({
 
 	const mainStore = useMainStore();
 
-	const tasks = mainStore.characters
-		.flatMap((char) => char.tasks)
-		.filter((task) => !task.completed);
-	const dailyTasks = tasks.filter((task) => task.type === "daily");
-	const weeklyTasks = tasks.filter((task) => task.type === "weekly");
-	const restedTasks = tasks.filter((task) => task.type === "rested");
+	const tasks = mainStore.characters.flatMap((char) => char.tasks);
+	const tasksByType = {
+		daily: tasks.filter((task) => task.type === "daily"),
+		weekly: tasks.filter((task) => task.type === "weekly"),
+		rested: tasks.filter((task) => task.type === "rested"),
+	};
+	const dailyTasks = tasksByType.daily.filter((task) => !task.completed);
+	const weeklyTasks = tasksByType.weekly.filter((task) => !task.completed);
+	const restedTasks = tasksByType.rested.filter((task) => !task.completed);
 
 	// Drag logic
 	const dragging = useRef(false);
@@ -97,12 +100,24 @@ export default function TodoCardsSeparateTasksButton({
 				Show Tasks
 			</Toggle>
 			<div className="grid grid-cols-[max-content_1fr] gap-1 text-xs text-muted-foreground">
-				<span className="text-end">{dailyTasks.length}</span>
-				<span>Daily</span>
-				<span className="text-end">{restedTasks.length}</span>
-				<span>Rested</span>
-				<span className="text-end">{weeklyTasks.length}</span>
-				<span>Weekly</span>
+				{tasksByType.daily.length > 0 && (
+					<>
+						<span className="text-end">{dailyTasks.length}</span>
+						<span>Daily</span>
+					</>
+				)}
+				{tasksByType.rested.length > 0 && (
+					<>
+						<span className="text-end">{restedTasks.length}</span>
+						<span>Rested</span>
+					</>
+				)}
+				{tasksByType.weekly.length > 0 && (
+					<>
+						<span className="text-end">{weeklyTasks.length}</span>
+						<span>Weekly</span>
+					</>
+				)}
 			</div>
 			<div
 				className="absolute bottom-1 right-1 cursor-move"
