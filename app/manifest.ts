e53f1ceaad219e938c2d2531678855bf4d113cad
lgpy <lgpy17@gmail.com>
2025-09-01 20:27:27 +0100

    pwa manifest

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Lost Ark Utils',
+    short_name: 'LoA Utils',
+    start_url: '/',
+    description: 'A set of utilities for Lost Ark',
+    display: 'fullscreen',
+    background_color: '#1e2030',
+    theme_color: '#181928',
+    icons: [
+      {
+        src: '/favicon_144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+    ],
+  }
+}
